c3fa0d5c53959ca8230743a92266bd937f12d731
EC2 Default User <ec2-user@ip-10-0-1-84.eu-west-3.compute.internal>
2025-06-13 16:12:08 +0000

    Proyecto CloudFlex

--- nav.php
+++ nav.php
@@ -2,10 +2,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">CloudFlex</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
@@ -20,6 +19,11 @@
                 <li class="nav-item">
                     <a class="nav-link" href="acerca_de.php">Acerca de</a>
                 </li>
+                <?php if (isset($_SESSION['id_usu'])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link text-danger" href="logout.php">Cerrar sesión</a>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
